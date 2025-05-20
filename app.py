6f02ad263b5a2c3caf830bbe435251aefcd0c3f5
aleffesanttos <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>
2025-05-20 16:44:23 -0300

    API - Rotas de Criar e Buscar tarefas

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-def hellou():
-  return "hellow"
+from flask import Flask, request, jsonify
+from models.task import Taks
 
-hellou()
-  
+app = Flask(__name__)
+
+tasks = []
+task_id = 1
+
+'''ROTA PARA CRIAR TAREFAS'''
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id
+  data = request.get_json()
+  new_task = Taks(id=task_id, title=data['title'],description=data.get('description', ''))
+  task_id += 1
+  tasks.append(new_task)
+  return jsonify({'message': 'Nova tarefas criada com Sucesso!'})
+
+
+'''ROTA PARA BUSCAR TAREFAS'''
+@app.route('/tasks', methods=['GET'])
+def search_tasks():
+  task_list = [Taks.to_dict() for task in tasks]
+  output = {        
+          "tasks": task_list,
+          "total_tasks" : 0
+  }
+
+if __name__ == "__main__":
+  app.run(debug=True)
