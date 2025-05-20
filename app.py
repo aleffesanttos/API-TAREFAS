286f298d92cb4153c900d8abb25b62ea9bf77059
aleffesanttos <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>
2025-05-20 17:19:33 -0300

    Review route GET

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ task_id = 1
 def create_task():
   global task_id
   data = request.get_json()
-  new_task = Taks(id=task_id, title=data['title'],description=data.get('description', ''))
+  new_task = Taks(id=task_id, title=data.get('title'), description=data.get('description'), completed=False)
   task_id += 1
   tasks.append(new_task)
   return jsonify({'message': 'Nova tarefas criada com Sucesso!'})
@@ -20,11 +20,12 @@ def create_task():
 '''ROTA PARA BUSCAR TAREFAS'''
 @app.route('/tasks', methods=['GET'])
 def search_tasks():
-  task_list = [Taks.to_dict() for task in tasks]
-  output = {        
+  task_list = [task.to_dict() for task in tasks]
+  output = {          
           "tasks": task_list,
           "total_tasks" : 0
   }
+  return jsonify(output)
 
 if __name__ == "__main__":
   app.run(debug=True)
