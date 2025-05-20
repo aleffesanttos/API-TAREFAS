31b075427ba8c43db1382fd83f191daa9d58d894
aleffesanttos <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>
2025-05-20 19:46:56 -0300

    Implementation of searching for tasks by title

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 tasks = []
 task_id = 1
 
-'''ROTA PARA CRIAR TAREFAS'''
+# Rota para Criar Tarefas
 @app.route('/tasks', methods=['POST'])
 def create_task():
   global task_id
@@ -17,15 +17,25 @@ def create_task():
   return jsonify({'message': 'Nova tarefas criada com Sucesso!'})
 
 
-'''ROTA PARA BUSCAR TAREFAS'''
+# Rota para buscar todas as tarefas
 @app.route('/tasks', methods=['GET'])
 def search_tasks():
-  task_list = [task.to_dict() for task in tasks]
-  output = {          
-          "tasks": task_list,
-          "total_tasks" : 0
-  }
-  return jsonify(output)
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(tasks)
+    }
+    return jsonify(output)
+
+# Rota para buscar tarefa por título
+@app.route('/tasks/<title>', methods=['GET'])
+def get_title(title):
+    for t in tasks:
+        if t.title == title:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possível encontrar esta tarefa!"}), 404
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
